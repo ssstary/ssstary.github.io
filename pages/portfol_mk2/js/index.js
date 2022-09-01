08f5940e261fb1ea4e91fdee4180fcf1dc90e8b0
ssstary <84666698+ssstary@users.noreply.github.com>
2022-09-01 13:31:31 +0900

    Add files via upload

--- pages/portfol_mk2/js/index.js
+++ pages/portfol_mk2/js/index.js
@@ -1,74 +1,65 @@
 $(document).ready(function () {
-  $('.h_btn').click(function(){
-		$(this).toggleClass('hbtn_on');
 
-		if($(this).hasClass('hbtn_on')){
-			$('.popmenu_wrap').fadeIn();
-		}else{
-			$('.popmenu_wrap').fadeOut();
-		};
-	});
+    $(".h_btn").click(function () {
+        $(this).toggleClass("hbtn_on");
 
-  var dir='next';
-  var num=0;
+        if ($(this).hasClass("hbtn_on")) {
+            $(".popmenu_wrap").fadeIn();
+        } else {
+            $(".popmenu_wrap").fadeOut();
+        }
+    });
 
-  $('.indicator_bar > button').first().addClass('on');
-  $('.indicator_bar > button').click(function(){
-    $('.indicator_bar > button').removeClass('on');
-    $(this).addClass('on');
-    var i=$(this).index();
-    var n=-(i*100);
-    $('.item_list').animate({
-      left: n+"%"
-    },'slow');
-  });
-  $('.btn.prev').click(function(){
-    dir=-'next';
-    num=num-1;
-    if(num<0){
-      num=5
-    }
-    $('.indicator_bar > button').eq(num).trigger('click')
-  });
-  $('.btn.next').click(function(){
-    dir='next';
-    num=num+1;
-    if(num>5){
-      num=0
-    }
-    $('.indicator_bar > button').eq(num).trigger('click')
+    $(".popmenu_wrap").click(function () {
+        $(".popmenu_wrap").fadeOut();
+        $(".h_btn").removeClass("hbtn_on");
+    });
 
-  });
+    var dir = "next",
+        num = 0;
+    var bgImgs = ["hyundai.jpg", "goldsalt.jpg", "koreauniv.jpg", "wbodam.jpg", "blueshark.jpg", "tabori.jpg"];
 
-  $(".item_list > li:first-child").mouseenter(function () {
-    $(".item_bg").css("background-color", "#211");
-  });
-  $(".item_list > li:nth-child(2)").mouseenter(function () {
-    $(".item_bg").css("background-color", "#923");
-  });
-  $(".item_list > li:nth-child(3)").mouseenter(function () {
-    $(".item_bg").css("background-color", "#461");
-  });
-  $(".item_list > li:nth-child(4)").mouseenter(function () {
-    $(".item_bg").css("background-color", "#015");
-  });
-  $(".item_list > li:nth-child(5)").mouseenter(function () {
-    $(".item_bg").css("background-color", "#198");
-  });
-  $(".item_list > li:last-child").mouseenter(function () {
-    $(".item_bg").css("background-color", "#501");
-  });
+    $(".item_bg").first().css("background-image", "url(./images/hyundai.jpg");
+    // $(".indicator_bar > li").first().addClass("on");
+    // $(".indicator_bar > li").click(function () {
+    //     $(".indicator_bar > li").removeClass("on");
+    //     $(this).addClass("on");
+    //     var i = $(this).index();
+    //     var n = -(i * 100);
+    //     $(".item_list").animate(
+    //         {
+    //             left: n + "%",
+    //         },
+    //         300
+    //     );
+    // });
+    $(".swiper-prev").click(function () {
+    //     dir = -"next";
+        num = num - 1;
+        if (num < 0) {
+            num = 5;
+        }
+        var imgIdx = bgImgs[num];
+        $(".item_bg").css("background-image", "url(./images/" + imgIdx);
+    //     $(".indicator_bar > li").eq(num).trigger("click");
+    });
+    $(".swiper-next").click(function () {
+    //     dir = "next";
+        num = num + 1;
+        if (num > 5) {
+            num = 0;
+        }
+        var imgIdx = bgImgs[num];
+        $(".item_bg").css("background-image", "url(./images/" + imgIdx);
+    //     $(".indicator_bar > li").eq(num).trigger("click");
+    });
 
 });
 
-
-
-
-
 $(window)
-  .resize(function () {
-    if (window.innerWidth < 500) {
-    } else {
-    }
-  })
-  .resize();
+    .resize(function () {
+        if (window.innerWidth < 500) {
+        } else {
+        }
+    })
+    .resize();
